d0a86687be3b86d3ee4f351da5ed4863b82687ce
ICE K. <info.icejourney@gmail.com>
2024-08-27 19:44:06 +0700

    update: EX1-6

--- ex-3.js
+++ ex-3.js
@@ -3,4 +3,12 @@ let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
 
-console.log(minScore);
+minScore = scores[0]; // เอา index ขึ้นมาสมมุติว่าต่ำที่สุดก่อนก่อน
+
+for (let i = 1; i < scores.length; i++) {
+    if (scores[i] < minScore) {
+        minScore = scores[i];
+    }
+}
+
+console.log(`คะแนนที่ต่ำที่สุดคือ = ${minScore}`);
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,7 @@
 let companyName = "TechUp";
 
 // Start coding here
+
+for (let text of companyName) {
+    console.log(` Number 1 character is ${text}`)
+}
